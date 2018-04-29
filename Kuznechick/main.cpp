39c34f56f81286264552d4393618f0a38f9ac173
Alexander Bocharov <alexbocharov2@gmail.com>
2018-04-29 22:39:49 +0300

    Corrected some errors

--- Kuznechick/main.cpp
+++ Kuznechick/main.cpp
@@ -4,10 +4,28 @@
 #include "stdafx.h"
 #include "Kuznechik.h"
 #include <iostream>
+#include <random>
+using namespace kuz;
+
+key_t GenerateMasterKey()
+{
+	//Инициализация генератора случайных чисел
+	int max_val = pow(2, 8) - 1;//случайный байт
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0, max_val);
+	key_t master_key;
+	for (int i = 0; i < master_key.size(); i++)
+	{
+		master_key[i] = dis(gen);
+	}
+	return master_key;
+}
 
 int main()
 {
-	Kuznechik kz;
+	key_t master_key=GenerateMasterKey();
+	Kuznechik kz(master_key);
 	const int data_size = 25;
 	byte data[data_size];
 	for (int i = 0; i < data_size; i++)
